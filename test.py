7189c6cdb078163c122081b575864de944529ac0
JiwonI788 <60461053+JiwonI788@users.noreply.github.com>
2021-03-03 02:32:35 +0900

    GdRi2Ge2

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import math
 import numpy as np
 
-print(np.dot(3,4))
+#print(np.dot(3,4))
+#SB3_vec=np.array([-1,-1,0])/np.linalg.norm([-1,-1,0])
+SB3_vec=np.array([-1,-1,0])/math.sqrt(2)
+print(SB3_vec)
